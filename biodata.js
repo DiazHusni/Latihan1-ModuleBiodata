4953bd3a4201460c0d9c4d0fa7718a5bb8b5b7af
Diaz Husni Fachriza <diazhusni04@gmail.com>
2023-05-09 22:21:18 +0700

    Biodata Node.js

---
+++ biodata.js
@@ -0,0 +1,9 @@
+var nama = "Diaz Husni Fachriza", 
+    tpLahir = "Bandung", 
+    tgLahir = "24 April 2005", 
+    alamat = "Blok Sakola No. 207 RT07 RW07, Kel. Melong, Kec. Cimahi Selatan, Kota Cimahi, Jawa Barat";
+
+console.log("Nama          : ", nama);
+console.log("Tempat Lahir  : ", tpLahir);
+console.log("Tanggal Lahir : ", tgLahir);
+console.log("Alamat        : ", alamat);
